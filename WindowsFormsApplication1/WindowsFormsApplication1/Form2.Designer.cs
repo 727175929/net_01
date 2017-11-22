ed68f465aad2cbdeeae5c30dab71fbe0111a2f34
727175929 <727175929@qq.com>
2017-11-22 10:48:12 +0800

    用timer控件实现了功能，但是还缺少一个接口函数，目前还有BUG

--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
@@ -39,6 +39,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -116,11 +118,26 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "清除日志";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(669, 446);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
@@ -147,5 +164,7 @@
         private System.Windows.Forms.Button button2;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication1
         IntPtr handle = IntPtr.Zero;
 
         Int16 dev_open = 0;  //设备是否连接
-        Int16 card_conn = 1;  //卡是否在读取的状态中  0表示不在读取  1表示读取中
+        Int16 card_conn = 0;  //卡是否在读取的状态中  0表示不在读取  1表示读取中
         string newLine;
         private string getErrMsg(int errCode)
         {
@@ -31,7 +31,6 @@ namespace WindowsFormsApplication1
             if (st < 0)
             {
                 //Console.WriteLine(getErrMsg(st));
-                //.Text = "连接读写器失败，错误原因：" + getErrMsg(st);
                 newLine = "连接读写器失败，错误原因：" + getErrMsg(st) + "\n";
                 richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
                 newLine = null;
@@ -48,16 +47,38 @@ namespace WindowsFormsApplication1
                 return true;
             }
         }
+
+        private void closecard(byte openMode, byte sector)   //openMode 打开模式 0x00 STD模式,只能寻到空闲状态下的卡片，被激活或停活（Halt）的卡片不会响应
+        {                                                     // 0x01 ALL模式,能寻到空闲状态和已经被停活（Halt）的卡片
+            byte[] cardUid = new byte[10];    //卡片ID
+            int st = cardReader.mwOpenCard(handle, openMode, cardUid);
+            if(st < 0 && card_conn == 1)
+            {
+                card_conn = 0;
+                timer1.Enabled = false;
+                newLine = "卡片已离开\n";
+                richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+                newLine = null;
+                System.Threading.Thread.Sleep(5 * 1000);//停2秒
+                timer2.Enabled = false;
+                timer1.Enabled = true;
+            }
+
+        }
         private void opencard(byte openMode, byte sector)   //openMode 打开模式 0x00 STD模式,只能寻到空闲状态下的卡片，被激活或停活（Halt）的卡片不会响应
         {                                                     // 0x01 ALL模式,能寻到空闲状态和已经被停活（Halt）的卡片
             byte[] cardUid = new byte[10];    //卡片ID
             //打开卡片
             int st = cardReader.mwOpenCard(handle, openMode, cardUid);
-            if (st < 0)
+            if (st < 0 )
             {
                 //newLine = "打开卡片失败，原因：" + getErrMsg(st) + "\n";
                 //richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
                 return;
+            }
+            else if (card_conn == 1)
+            {
+                timer1.Enabled = false;
             }
             else
             {
@@ -66,8 +87,14 @@ namespace WindowsFormsApplication1
                 newLine = "卡片ID：" + cardUidStr.ToString() + "\n";
                 richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
                 newLine = null;
+                card_conn = 1;  //标识符表示处于连接状态
             }
             //System.Threading.Thread.Sleep(2 * 1000);//停2秒
+            newLine = "读取卡片成功,等待卡片离开\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = null;
+            timer1.Enabled = false;
+            timer2.Enabled = true;
         }
         public Form2()
         {
@@ -89,9 +116,11 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Enabled = false;
+            newLine = "开始识别卡片\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
             //opencard(1, 2);//M1操作，对2扇区进行操作
-            
+            timer1.Enabled = true;  //当开始识别卡片时，调用计时器
+           
             
         }
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -105,8 +134,6 @@ namespace WindowsFormsApplication1
                 button1.Enabled = true;
             else
                 button1.Enabled = false;
-
-            timer1.Enabled = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -115,7 +142,20 @@ namespace WindowsFormsApplication1
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer1.Interval = 1000;//毫秒为单位
+            timer1.Interval = 5000;//毫秒为单位
+            timer2.Interval = 5000;
         }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            closecard(1, 2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+
     }
 }
